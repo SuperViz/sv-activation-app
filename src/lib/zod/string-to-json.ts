8751122a992e1d89bfe6443adc266462bc9a4a85
Carlos <carlos.santos74@outlook.com>
2024-08-22 14:26:51 -0300

    chore: implement zod generic helpers

---
+++ src/lib/zod/string-to-json.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+export const StringToJSONSchema = z.string()
+  .transform((str, ctx): z.infer<ReturnType<typeof JSON.parse>> => {
+    try {
+      return JSON.parse(str)
+    } catch (e) {
+      ctx.addIssue({ code: 'custom', message: 'Invalid JSON' })
+      return z.NEVER
+    }
+  })
