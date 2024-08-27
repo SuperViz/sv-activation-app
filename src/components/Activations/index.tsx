f348b61b917a0df6aee91fb62465dc1aee572da6
Daniela Caus <38079926+danicaus@users.noreply.github.com>
2024-08-27 19:48:31 -0300

    feat(dashboard): ajusta layout para nova versão

--- src/components/Activations/index.tsx
+++ src/components/Activations/index.tsx
@@ -8,12 +8,10 @@ interface IActivationsProps {
 export default function Activations({ activations }: IActivationsProps) {
   function Activation({activation}: { activation: IActivation }) {
     return (
-      <div className="mt-6 tv:mt-[3.125rem] flex items-center" key={activation.activationWord}>
+      <div className="flex items-center ml-[2rem] tv:ml-[4rem]">
         <Circle color={activation.color} />
         <span className="text-2xl tv:text-[3rem] tv:leading-[4rem] font-black">
-        {`${activation.description} `}
-          <ActivationWord text={activation.activationWord} color={activation.color}/>
-          {activation.subtext && <ActivationSubtext subtext={activation.subtext} color={activation.color} /> }
+        {activation.activationWord}
         </span>
       </div>
     )
@@ -21,42 +19,20 @@ export default function Activations({ activations }: IActivationsProps) {
 
   function Circle({ color }: { color: string}) {
     return (
-      <span className="mr-3 tv:mr-[1.625rem] activations">
-      <div className="pinpoint">
-        <div className="content"></div>
-        <div className="pinpoint-ring">
-          <svg viewBox="0 0 78 78">
-            <circle className="circle" r="33" cx="39" cy="39" fill="#ffffff1a" stroke={`#${color}`}></circle>
-          </svg>
-        </div>
-      </div>
+    <span className="mr-[1rem] tv:mr-[1.875rem]">
+      <svg width="60" height="60" viewBox="0 0 60 60" fill="none" className="w-[1.875rem] h-[1.875rem] tv:w-[3.75rem] tv:h-[3.75rem]">
+        <circle cx="30" cy="30" r="25" stroke={`#${color}`} strokeWidth="10"/>
+      </svg>
     </span>
-    )
-  }
-
-  function ActivationSubtext({subtext, color} : { subtext: Required<IActivation>['subtext'], color: string }) {
-    return (
-      <>
-        <br/>
-        <span className="text-lg tv:text-[2.25rem] tv:leading-[2.65rem]">{`${subtext.description} `}
-          <ActivationWord text={subtext.activationWord} color={color} />
-      </span>
-      </>
-    )
-  }
-  
-  function ActivationWord({text, color}: { text: string, color: string }) {
-    return (
-      <span style={{color: `#${color}`}}>{text}</span>
     )
   }
   
   return (
-    <div className="p-8 bg-[#7e7a88]/10 rounded-2xl h-fit tv:p-[4rem]">
+    <div className="bg-[#7e7a88]/10 flex items-center px-[2.25rem] py-[1rem] rounded-2xl tv:px-[4.5rem] tv:py-[1.875rem] tv:rounded-[2rem]">
       <p className="text-xl tv:text-[2.75rem] tv:leading-[3.2rem]">Ativações</p>
-      {activations.map((act) => (
-        <Activation key={act.color} activation={act} />
-      ))}
+        {activations.map((act) => (
+          <Activation key={act.color} activation={act} />
+        ))}
     </div>
   )
 }
