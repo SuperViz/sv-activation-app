ebeb6966714714e903d7a2385ae3175baf7b80f9
Carlos <carlos.santos74@outlook.com>
2024-09-02 08:00:17 -0300

    build fix

--- src/components/UsersDashboard/index.tsx
+++ src/components/UsersDashboard/index.tsx
@@ -28,8 +28,6 @@ export default function UsersDashboard() {
   const engineRef = useRef<Matter.Engine | null>(null);
   const ballsRef = useRef<Ball[]>([]);
 
-  const { subscribe } = useRealtime('default');
-
   React.useEffect(() => {
     subscribe("activation", (e) => console.log('evento!', e));
   }, []);
