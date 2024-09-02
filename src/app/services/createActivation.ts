577c3d7493d1d0ad6ef839a065617e921c660e4f
Vitor Norton <vtnorton@outlook.com>
2024-09-02 09:18:50 -0300

    fixed pr issues

--- src/app/services/createActivation.ts
+++ src/app/services/createActivation.ts
@@ -1,16 +1,13 @@
-import {ActivationType} from "@/global/global.types";
+import { ActivationType } from "@/global/global.types";
 
 interface ICreateActivationProps {
-  name: ActivationType, 
+  name: ActivationType,
   userEmail: string
 }
 
-export async function createActivation(props : ICreateActivationProps): Promise<void> {
+export async function createActivation(props: ICreateActivationProps): Promise<void> {
   await fetch('/api/user/activation', {
     method: 'POST',
     body: JSON.stringify(props),
   })
-    .then(async (res) => {
-      return res.json()
-    })
 }
