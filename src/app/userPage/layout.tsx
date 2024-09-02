d4cbdba46965876b7ae6601d885a492a30fed1a2
Vitor Norton <vtnorton@outlook.com>
2024-09-02 09:07:27 -0300

    layout adjust

--- src/app/userPage/layout.tsx
+++ src/app/userPage/layout.tsx
@@ -10,7 +10,7 @@ export default function ActivationsLayout({
 }>) {
   return (
     <div className='flex flex-col w-full h-screen mobileBg'>
-      <div className="relative flex h-full overflow-auto overflow-x-hidden px-8 py-6 flex-col justify-between items-center z-10">
+      <div className="relative flex h-full overflow-auto overflow-x-hidden px-8 py-6 flex-col items-center z-10">
         <Image src="/logo-sm.svg" width={109} height={80} alt="Logo Superviz" />
         {children}
       </div>
