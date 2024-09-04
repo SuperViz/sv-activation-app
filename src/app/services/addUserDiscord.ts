aa672a7b720c8a010110b65cd6d094e23272a753
Ian Silva <raspincel@gmail.com>
2024-09-04 15:19:35 -0300

    fix: pass email discord

--- src/app/services/addUserDiscord.ts
+++ src/app/services/addUserDiscord.ts
@@ -1,16 +1,17 @@
-import { getUserData } from './getUserData';
+import { getUserData } from "./getUserData";
 
-export async function addUserDiscord(formData: Record<string, string>): Promise<void> {
-  await fetch('/api/user', {
-    headers: { cache: 'no-store' },
-    method: 'PATCH',
+export async function addUserDiscord(
+  formData: Record<string, string>
+): Promise<void> {
+  await fetch("/api/user", {
+    headers: { cache: "no-store" },
+    method: "PATCH",
     body: JSON.stringify(formData),
-  })
-    .then(async (res) => {
-      if (res.status === 409) {
-        return await getUserData(formData['email']);
-      } else if (!res.ok) {
-        throw new Error('Erro');
-      }
-    })
+  }).then(async (res) => {
+    if (res.status === 409) {
+      return await getUserData(formData["email"]);
+    } else if (!res.ok) {
+      throw new Error("Erro");
+    }
+  });
 }
